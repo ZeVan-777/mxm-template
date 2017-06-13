8c487f6179a4ae011a56232f8612784bd436d5a1
ZeVan <zhengyifan@coracle.com>
2017-06-13 18:34:07 +0800

    move lib files in /bower_components to /src/public via gulp

--- gulpfile.js
+++ gulpfile.js
@@ -7,13 +7,13 @@ const	browserSync = require('browser-sync').create(),
 
 const lessDir = path.join(__dirname, 'src/less'),
 			tplDir = path.join(__dirname, 'src/template'),
-			cssDir = path.join(__dirname, 'src/css');
+			cssDir = path.join(__dirname, 'src/css'),
+			publicDir = path.join(__dirname, 'src/public');
 
 const lessPath = path.join(lessDir, '**.less'),
 			tplPath = path.join(tplDir, '**.html');
 
-gulp.task('less', () => {	
-	console.log('less');
+gulp.task('less', () => {
 	return gulp.src(lessPath)
 	  .pipe(gulpLess())
 	  .pipe(gulp.dest(cssDir))
@@ -23,25 +23,38 @@ gulp.task('less', () => {
 });
 
 gulp.task('html', () => {
-	return gulp.src(tplPath)
+	return gulp.src(path.join(tplDir, '*.html'))
 		.pipe(gulpFileInclude({
 			prefix: '@@',
 			basepath: '@file'	// for resolving path passed to @@include method 
 		}))
-		.pipe(gulp.dest('src'));
+		.pipe(gulp.dest('src'))
+		.pipe(reload({
+			stream: true
+		}));
 });
 
-gulp.task('serve', ['less', 'html'], () => {		
+// move bower files to src directory
+const bowerList = [
+	'zui/dist/css/zui.css',
+	'normalize-css/normalize.css',
+	'html5shiv/dist/html5shiv.min.js',
+	'jquery/jquery.min.js'
+];
+gulp.task('bower', () => {
+	return gulp.src(bowerList.map(item => `bower_components/${item}`))
+	  .pipe(gulp.dest(publicDir));
+});
+
+gulp.task('serve', ['less', 'html', 'bower'], () => {		
 	browserSync.init({
 		server: {
-			baseDir: path.join(__dirname, 'src')
+			baseDir: 'src'
 		}
 	});
 
-	gulp.watch(lessPath, ['less'], (evt) => {
-		console.log(evt);
-	});	
+	gulp.watch(lessPath, ['less']);	
 	gulp.watch(tplPath, ['html']);
 });
 
-gulp.task('default', ['serve'], () => {});
+gulp.task('default', ['serve']);
